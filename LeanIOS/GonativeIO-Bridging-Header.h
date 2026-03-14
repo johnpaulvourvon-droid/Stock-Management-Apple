e089fe13297d6adec522aa218124b5c3ca243d8c
johnpaulvourvon-droid <johnpaulvourvon@gmail.com>
2026-03-14 14:17:52 +0800

    Update GonativeIO-Bridging-Header.h remove Go native

--- LeanIOS/GonativeIO-Bridging-Header.h
+++ LeanIOS/GonativeIO-Bridging-Header.h
@@ -2,7 +2,6 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import <GoNativeCore/GoNativeCore.h>
 #import "LEANUtilities.h"
 #import "LEANWebViewController.h"
 #import "LEANRootViewController.h"
